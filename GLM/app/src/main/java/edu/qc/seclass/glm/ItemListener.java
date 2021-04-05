fd4abb70097d0714a9197c05f6435fe040ea133d
sihangchen <SIHANG.CHEN57@qmail.cuny.edu>
2021-04-05 11:54:12 -0400

    Added RecylerView not fully funciton

---
+++ GLM/app/src/main/java/edu/qc/seclass/glm/ItemListener.java
@@ -0,0 +1,7 @@
+package edu.qc.seclass.glm;
+
+import java.util.ArrayList;
+
+public interface ItemListener {
+    void onItemChange(ArrayList<String> arrayList);
+}
--- GLM/app/src/main/java/edu/qc/seclass/glm/MyItems.java
+++ GLM/app/src/main/java/edu/qc/seclass/glm/MyItems.java
@@ -1,6 +1,8 @@
 package edu.qc.seclass.glm;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,9 +11,13 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.widget.Button;
 
-public class MyItems extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MyItems extends AppCompatActivity implements ItemListener {
     Button addButton;
     String listName;
+    RecyclerView recycler_view;
+    ItemAdapter itemAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +25,28 @@ public class MyItems extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         listName = getIntent().getStringExtra("ListViewClickValue");
         setContentView(R.layout.activity_my_items);
+
+        recycler_view = findViewById(R.id.recyler_view);
+
+        setRecyclerView();
     }
+
+    private ArrayList<String> getItemData(){
+        ArrayList<String> arrayList = new ArrayList<>();
+        arrayList.add("Item 1");
+        arrayList.add("Item 2");
+        arrayList.add("Item 3");
+        arrayList.add("Item 4");
+        return arrayList;
+    }
+
+    private void setRecyclerView() {
+        recycler_view.setHasFixedSize(false);
+        recycler_view.setLayoutManager(new LinearLayoutManager(this));
+        itemAdapter = new ItemAdapter(this,getItemData(),this);
+        recycler_view.setAdapter(itemAdapter);
+    }
+
 
     public void goSearchCategoryPage(View view){
         Intent i = new Intent(this,searchCategory.class);
@@ -43,5 +70,10 @@ public class MyItems extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.itemmenu,menu);
         return true;
+    }
+
+    @Override
+    public void onItemChange(ArrayList<String> arrayList) {
+
     }
 }
