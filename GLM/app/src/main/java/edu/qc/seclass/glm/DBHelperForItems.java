018176cebb79eef4cdfe1d442f6d61221aeb1392
colinseeworld <linlinzhu33@gmail.com>
2021-04-05 04:51:32 -0400

    Insert data into database

--- GLM/app/src/main/java/edu/qc/seclass/glm/DBHelperForItems.java
+++ GLM/app/src/main/java/edu/qc/seclass/glm/DBHelperForItems.java
@@ -24,9 +24,83 @@ public class DBHelperForItems extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
 
-        String createTableStatement = "CREATE TABLE " + DB_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " TEXT, " + CATEGORY + " TEXT)";
-        //String createTableStatement = "CREATE TABLE ITEM_LIST (ID INTEGER PRIMARY KEY AUTOINCREMENT, ITEM_NAME TEXT, ITEM_CATEGORY TEXT)"
+//        String createTableStatement = "CREATE TABLE " + DB_TABLE + " (" + ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " + NAME + " TEXT, " + CATEGORY + " TEXT)";
+//        db.execSQL(createTableStatement);
+
+        String createTableStatement = ("CREATE TABLE IF NOT EXISTS " + DB_TABLE + "(ID INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, CATEGORY TEXT)");
         db.execSQL(createTableStatement);
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Strawberries','Fruits')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Blueberries','Fruits')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Papaya','Fruits')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Watermelon','Fruits')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Cherries','Fruits')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Oranges','Fruits')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Chicken', 'Meat')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Sausage', 'Meat')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Beef', 'Meat')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Pork', 'Meat')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Duck', 'Meat')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Cheese','Dairy')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Milk','Dairy')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Yogurt','Dairy')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Cream','Dairy')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Butter','Dairy')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Tomatoes','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Carrots','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Potatos','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Broccoli','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Cucumber','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Eggplant','Vegetable')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Spring Onions','Vegetable')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Crossiant','Bakery')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Cake','Bakery')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Oreos','Bakery')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Donuts','Bakery')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Baguette','Bakery')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('UTZ','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Takis','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Flamin hot Cheetos','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Doritos','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Nuts','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Hershey','Snack')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Ferrero Rocher','Snack')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Wine','Liquor')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Heineken','Liquor')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Beer','Liquor')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Soju','Liquor')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Tequila','Liquor')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Coca Cola','Beverage')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Tropicana Orange juice','Beverage')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Pepsi','Beverage')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Fiji water','Beverage')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Snapple','Beverage')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Clorox','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Bounty','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Paper Towels','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Dish Washing Soap','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Windex','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Tissues','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Charmin','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Garbage Bags','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Broom','Cleaning')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Dustpan','Cleaning')");
+
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Shampoo','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Body Wash','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Soap','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Conditioner','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Face Wash','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Mouth Wash','Hygiene')");
+        db.execSQL("INSERT INTO " + DB_TABLE + "(NAME,CATEGORY) VALUES ('Toothpaste','Hygiene')");
 
     }
 
